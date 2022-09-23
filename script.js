35aeef76583ae9046d280a187083351894407356
iamoen <owen.corona6@gmail.com>
2022-09-23 22:19:59 +0800

    Add hover effects on JS instead on CSS

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //adds divs on DOM
 const container = document.querySelector("#container");
 for (let index = 0; index <= 255; index++) {
-    container.innerHTML += '<div class= "grid" > </div>';
+    container.innerHTML += '<div class= "grid" onmouseover = "draw(this)"> </div>';
 }
 
-
+function draw(element){
+    element.style.cssText = "background-color: green;";
+}
