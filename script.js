9122072c2f2f55ec0e79fd1d0aad201026783c7f
iamoen <owen.corona6@gmail.com>
2022-09-23 23:15:18 +0800

    Added grid size editor

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //adds divs on DOM
-const container = document.querySelector("#container");
+const container = document.querySelector("#canvas-wrapper");
 for (let index = 0; index <= 255; index++) {
     container.innerHTML += '<div class= "grid" onmouseover = "draw(this)"> </div>';
 }
