342be432c0a40dd2df99048e8bf89624d01fc631
iamoen <owen.corona6@gmail.com>
2022-09-23 23:32:26 +0800

    Added a function that fits the grid to the canvas

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+//get grid size
+const gridDimension = Number(document.getElementById('gridNumber').value);
+
 //adds divs on DOM
 const container = document.querySelector("#canvas-wrapper");
-for (let index = 0; index <= 255; index++) {
+for (let index = 0; index <= gridDimension; index++) {
     container.innerHTML += '<div class= "grid" onmouseover = "draw(this)"> </div>';
 }
+container.style.cssText = "grid-template-columns: repeat("+gridDimension+", 1fr); grid-template-rows: repeat("+gridDimension+",1fr);"
 
 function draw(element){
     element.style.cssText = "background-color: green;";
 }
+
