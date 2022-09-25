b77ee1e77f754ab91380b0d4e0cdfc150c82c89a
iamoen <owen.corona6@gmail.com>
2022-09-25 21:42:38 +0800

    Added functions on color picker & random color

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+function getGridSize(){
+    return Number(document.getElementById('gridNumber').value);
+}
+
+
 function loadGrid(gridDimension){
     const container = document.querySelector("#canvas-wrapper");
     for (let index = 0; index <= gridDimension * gridDimension; index++) {
-        container.innerHTML += '<div class= "grid" onclick = "draw(this)"> </div>';
+        container.innerHTML += '<div class= "grid" onmouseover = "draw(this)"> </div>';
     }
     container.style.cssText = "grid-template-columns: repeat("+gridDimension+", 1fr); grid-template-rows: repeat("+gridDimension+",1fr);"
     let childDiv = document.querySelectorAll(".grid");
@@ -11,29 +16,40 @@ function loadGrid(gridDimension){
 }
 
 function addGrid(){
-    let gridDimension = Number(document.getElementById('gridNumber').value);
-    if (gridDimension == 64) {
+    let size = getGridSize();
+    if (size == 64) {
         
     }else{
-        gridDimension++;
-        document.getElementById('gridNumber').value = gridDimension;
+        size++;
+        document.getElementById('gridNumber').value = size;
     }
-    loadGrid(gridDimension);
+    loadGrid(size);
 }
 
 function minusGrid(){
-    let gridDimension = Number(document.getElementById('gridNumber').value);
+    let size = getGridSize();
     if (gridDimension == 0) {
         
     }else{
-        gridDimension--;
-        document.getElementById('gridNumber').value = gridDimension;
+        size--;
+        document.getElementById('gridNumber').value = size;
     }
-    loadGrid(gridDimension);
+    loadGrid(size);
+}
+
+function getColor(){
+    return document.getElementById("color-picker").value;
 }
 
 function draw(element){
-    element.style.cssText = "background-color: green;";
+    const RandomColor = document.getElementById("randomColor").checked;
+    if (RandomColor) {
+        var randomColor = Math.floor(Math.random()*16777215).toString(16);
+        element.style.cssText = "background-color: #" + randomColor;
+    }else{
+        element.style.cssText = "background-color: " + getColor();
+    }
 }
 
-loadGrid(16);
+//default grid size
+loadGrid(getGridSize());
