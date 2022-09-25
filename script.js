d7b48aff1204b1754c526f3402a8dbc4f18cedd2
iamoen <owen.corona6@gmail.com>
2022-09-25 19:22:19 +0800

    Merged grid loader into one function

--- script.js
+++ script.js
@@ -1,44 +1,39 @@
-
-
-//get grid size
-let gridDimension = Number(document.getElementById('gridNumber').value);
-//adds divs on DOM
-const container = document.querySelector("#canvas-wrapper");
-for (let index = 0; index <= gridDimension * gridDimension; index++) {
-    container.innerHTML += '<div class= "grid" onclick = "draw(this)"> </div>';
-}
-container.style.cssText = "grid-template-columns: repeat("+gridDimension+", 1fr); grid-template-rows: repeat("+gridDimension+",1fr);"
-
-
-function gridEditor(){
-    let gridDimension = Number(document.getElementById('gridNumber').value);
+function loadGrid(gridDimension){
+    const container = document.querySelector("#canvas-wrapper");
     for (let index = 0; index <= gridDimension * gridDimension; index++) {
         container.innerHTML += '<div class= "grid" onclick = "draw(this)"> </div>';
     }
-
+    container.style.cssText = "grid-template-columns: repeat("+gridDimension+", 1fr); grid-template-rows: repeat("+gridDimension+",1fr);"
     let childDiv = document.querySelectorAll(".grid");
     childDiv.forEach(grid => {
         grid.style.cssText = "background-color: white;";
     });
-
-    gridDimension = Number(document.getElementById('gridNumber').value);
-    container.style.cssText = "grid-template-columns: repeat("+gridDimension+", 1fr); grid-template-rows: repeat("+gridDimension+",1fr);"
 }
 
 function addGrid(){
     let gridDimension = Number(document.getElementById('gridNumber').value);
-    document.getElementById("gridNumber").value = gridDimension + 1;
+    if (gridDimension == 64) {
+        
+    }else{
+        gridDimension++;
+        document.getElementById('gridNumber').value = gridDimension;
+    }
+    loadGrid(gridDimension);
 }
 
 function minusGrid(){
     let gridDimension = Number(document.getElementById('gridNumber').value);
-    document.getElementById("gridNumber").value = gridDimension - 1;
+    if (gridDimension == 0) {
+        
+    }else{
+        gridDimension--;
+        document.getElementById('gridNumber').value = gridDimension;
+    }
+    loadGrid(gridDimension);
 }
-
-
-
-
 
 function draw(element){
     element.style.cssText = "background-color: green;";
 }
+
+loadGrid(16);
